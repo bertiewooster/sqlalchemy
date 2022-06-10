bf9c1e7d8d41f7f14362ffbb8736b838934bb2a8
Jeremy Monat <jemonat@calalum.org>
2022-06-10 14:25:28 -0400

    Read data from table

--- main.py
+++ main.py
@@ -9,20 +9,27 @@ engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 #     result = conn.execute(text("select 'hello world'"))
 #     print(result.all())
 
+# Create table
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+
 # Insert data and then commit changes
-# with engine.connect() as conn:
-#     conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-#     conn.execute(
-#         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-#         [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
-#     )
-#     conn.commit()
+with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
+    conn.commit()
 
 # "Begin once": Declare to be a transaction block
 #   to automatically COMMIT if successful, or ROLLBACK if exception raised
 with engine.begin() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
     conn.execute(
         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
         [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
     )
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
