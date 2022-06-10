e6823c5afdf6ff16c05f5e802a3ad53505915349
Jeremy Monat <jemonat@calalum.org>
2022-06-10 14:22:35 -0400

    "Begin once" to automatically create transaction

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from sqlalchemy import create_engine, text
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
+# Trivial connection
+# with engine.connect() as conn:
+#     result = conn.execute(text("select 'hello world'"))
+#     print(result.all())
 
-with engine.connect() as conn:
+# Insert data and then commit changes
+# with engine.connect() as conn:
+#     conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+#     conn.execute(
+#         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+#         [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+#     )
+#     conn.commit()
+
+# "Begin once": Declare to be a transaction block
+#   to automatically COMMIT if successful, or ROLLBACK if exception raised
+with engine.begin() as conn:
     conn.execute(text("CREATE TABLE some_table (x int, y int)"))
     conn.execute(
         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
     )
-    conn.commit()
